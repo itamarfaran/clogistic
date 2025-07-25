2b0dfda713eb1fed3d390204b8326f2dbc937e5b
Itamar Faran <itamar@fidocredit.com>
2025-07-25 22:11:20 +0300

    refactor tests4

--- tests/test_logistic.py
+++ tests/test_logistic.py
@@ -10,6 +10,40 @@ from clogistic import ConstrainedLogisticRegression
 from tests.conftest import fake_data
 
 
+def bounds_and_constraints(
+    p: int,
+    fit_intercept: bool = True,
+) -> tuple[Bounds, LinearConstraint]:
+    if fit_intercept:
+        p = p + 1
+
+    lb = np.r_[np.full(p - 1, -1), -np.inf]
+    ub = np.r_[np.zeros(p - 1), np.inf]
+    bounds = Bounds(lb, ub)
+
+    lb = np.array([0.0])
+    ub = np.array([0.5])
+    A = np.zeros((1, p))
+    constraints = LinearConstraint(A, lb, ub)
+
+    return bounds, constraints
+
+
+def assert_predictions(
+    clf: LogisticRegression, X: np.ndarray, y: np.ndarray, value: float = 0.9
+) -> None:
+    assert np.all(np.unique(y) == clf.classes_)
+
+    pred = clf.predict(X)
+    assert np.mean(pred == y) > value
+
+    probabilities = clf.predict_proba(X)
+    assert probabilities.sum(axis=1) == pytest.approx(np.ones(X.shape[0]))
+
+    pred = clf.classes_[np.argmax(clf.predict_log_proba(X), axis=1)]
+    assert np.mean(pred == y) > value
+
+
 @pytest.mark.parametrize(
     "kwargs",
     [
@@ -40,19 +74,10 @@ def test_parameters(fake_data, kwargs):
 )
 def test_solver(fake_data, clf):
     X, y, _ = fake_data
-
-    lb = np.r_[np.full(X.shape[1], -1), -np.inf]
-    ub = np.r_[np.zeros(X.shape[1]), np.inf]
-    bounds = Bounds(lb, ub)
+    bounds, constraints = bounds_and_constraints(X.shape[1])
 
     with pytest.raises(ValueError):
         clf.fit(X, y, bounds=bounds)
-
-    lb = np.array([0.0])
-    ub = np.array([0.5])
-    A = np.zeros((1, X.shape[1] + 1))
-    A[0, :2] = np.array([-1, 1])
-    constraints = LinearConstraint(A, lb, ub)
 
     with pytest.raises(ValueError):
         clf.fit(X, y, constraints=constraints)
@@ -70,24 +95,23 @@ def test_target(fake_data):
 
 def test_bounds_and_constraints(fake_data):
     X, y, _ = fake_data
-    lb = np.r_[np.full(X.shape[1] - 1, -1), -np.inf]
-    ub = np.r_[np.zeros(X.shape[1] - 1), np.inf]
+    bounds, constraints = bounds_and_constraints(X.shape[1], fit_intercept=False)
 
     with pytest.raises(TypeError):
-        ConstrainedLogisticRegression(penalty="l2").fit(X, y, bounds=[(-np.inf, np.inf)] * X.shape[1])
+        ConstrainedLogisticRegression(penalty="l2").fit(
+            X, y, bounds=[(-np.inf, np.inf)] * X.shape[1]
+        )
 
     with pytest.raises(ValueError):
-        ConstrainedLogisticRegression(penalty="l2").fit(X, y, bounds=Bounds(lb, ub))
-
-    lb = np.array([0.0])
-    ub = np.array([0.5])
-    A = np.zeros((1, X.shape[1]))
+        ConstrainedLogisticRegression(penalty="l2").fit(X, y, bounds=bounds)
 
     with pytest.raises(TypeError):
-        ConstrainedLogisticRegression().fit(X, y, constraints=[A, lb, ub])
+        ConstrainedLogisticRegression().fit(
+            X, y, constraints=[constraints.A, constraints.lb, constraints.ub]
+        )
 
     with pytest.raises(ValueError):
-        ConstrainedLogisticRegression().fit(X, y, constraints=LinearConstraint(A, lb, ub))
+        ConstrainedLogisticRegression().fit(X, y, constraints=constraints)
 
 
 @pytest.mark.parametrize(
@@ -104,18 +128,8 @@ def test_predict_breast_cancer(breast_cancer_data, solver, penalty, fit_intercep
     # Test constrained logistic regression with the breast cancer dataset
     # Test that all solvers with all regularizations score (>0.93) for the training data
     clf = ConstrainedLogisticRegression(solver=solver, penalty=penalty, l1_ratio=0.5)
-
     clf.fit(X, y)
-    assert np.all(np.unique(y) == clf.classes_)
-
-    pred = clf.predict(X)
-    assert np.mean(pred == y) > 0.93
-
-    probabilities = clf.predict_proba(X)
-    assert probabilities.sum(axis=1) == pytest.approx(np.ones(X.shape[0]))
-
-    pred = clf.classes_[np.argmax(clf.predict_log_proba(X), axis=1)]
-    assert np.mean(pred == y) > 0.9
+    assert_predictions(clf, X, y)
 
 
 @pytest.mark.parametrize(
@@ -128,31 +142,13 @@ def test_predict_breast_cancer(breast_cancer_data, solver, penalty, fit_intercep
 def test_predict_breast_cancer_bounds_constraints(breast_cancer_data, solver, penalty):
     # Test constrained logistic regression with the breast cancer dataset
     X, y = breast_cancer_data
-
-    lb = np.r_[np.full(X.shape[1], -1), -np.inf]
-    ub = np.r_[np.zeros(X.shape[1]), np.inf]
-    bounds = Bounds(lb, ub)
-
-    lb = np.array([0.0])
-    ub = np.array([0.5])
-    A = np.zeros((1, X.shape[1] + 1))
-    A[0, :2] = np.array([-1, 1])
-    constraints = LinearConstraint(A, lb, ub)
+    bounds, constraints = bounds_and_constraints(X.shape[1])
 
     # Test that all solvers with all regularizations score (>0.93) for the training data
     clf = ConstrainedLogisticRegression(solver=solver, penalty=penalty, l1_ratio=0.5)
 
     clf.fit(X, y, bounds=bounds, constraints=constraints)
-    assert np.all(np.unique(y) == clf.classes_)
-
-    pred = clf.predict(X)
-    assert np.mean(pred == y) > 0.93
-
-    probabilities = clf.predict_proba(X)
-    assert probabilities.sum(axis=1) == pytest.approx(np.ones(X.shape[0]))
-
-    pred = clf.classes_[np.argmax(clf.predict_log_proba(X), axis=1)]
-    assert np.mean(pred == y) > 0.9
+    assert_predictions(clf, X, y)
 
 
 @pytest.mark.parametrize("solver", ("ecos", "scs"))
